4bc53eb3d220fb16d9414243dd40a40bda308f1f
David Gutarra R <davidgutarraramos@gmail.com>
2025-07-30 02:46:23 -0500

    refactor: configure class to return steps in script

--- src/com/sware/core/config/AgentManager.groovy
+++ src/com/sware/core/config/AgentManager.groovy
@@ -2,7 +2,7 @@ package com.sware.core.config
 
 import com.sware.core.services.LogManager
 
-public class AgentManager {
+class AgentManager {
     public static void configureAgent() {
         LogManager.info("Configuring agent: local_worker")
         this.agent { label 'local_worker' }
--- src/development/MavenJUnitTest.groovy
+++ src/development/MavenJUnitTest.groovy
@@ -8,44 +8,60 @@ import com.sware.core.tasks.PostExecution
 
 class MavenJUnitTest {
     def static logger = LogManager
+    protected script
     private static String karateOpts = ""
     private static String jira = "-Djxray.update.evidence=true"
 
+    MavenJUnitTest(script) {
+        this.script = script
+    }
+
     public void checkTools() {
-        this.script.steps.sh 'mvn -v'
-        this.script.steps.sh 'java --version'
+        this.steps.sh {
+            'mvn -v'
+            'java --version'
+        } 
     }
 
-    public static void configureEnv(String env) {
-        AgentManager.configureAgent()
-        EnvironmentManager.setupEnvironment(env)
-
-    }
-
-    public static void installDependencies() {
-        this.script.steps.sh 'mvn clean install -DskipTests'
-    }
-
-    public static void compileProject() {
-        this.script.steps.sh 'mvn compile test-compile'
-    }
-
-    public static void runTestKarate(String tags) {
-        logger.info("fetching comand to test ...")
-
-        String runEnv = EnvironmentManager.getEnv()
-        if (runEnv.equals("qa")) {
-            runEnv = "cert"
+    public void configureEnv(String env) {
+        this.script.steps {
+            AgentManager.configureAgent()
+            EnvironmentManager.setupEnvironment(env)
         }
-        karateOpts = "\"mvn test -Dkarate.options=--tags ${tags}\" -Dkarate.env=${runEnv}"
-        karateOpts += CredentialManager.loadCredentials()
-        jira += CredentialManager.loadJiraXrayCredentials()
-        String command = "${karateOpts} ${jira}"
-        logger.info("Running tests ...")
-        this.script.steps.sh "${command}"
     }
 
-    public static void runPostTask() {
-        PostExecution.runPostTask()
+    public void installDependencies() {
+        this.steps.sh {
+            'mvn clean install -DskipTests'
+        }
+    }
+
+    public void compileProject() {
+        this.steps.sh {
+            'mvn compile test-compile'
+        }
+    }
+
+    public void runTestKarate(String tags) {
+        this.steps {
+            logger.info("fetching comand to test ...")
+
+            String runEnv = EnvironmentManager.getEnv()
+            if (runEnv.equals("qa")) {
+                runEnv = "cert"
+            }
+            karateOpts = "\"mvn test -Dkarate.options=--tags ${tags}\" -Dkarate.env=${runEnv}"
+            karateOpts += CredentialManager.loadCredentials()
+            jira += CredentialManager.loadJiraXrayCredentials()
+            String command = "${karateOpts} ${jira}"
+            logger.info("Running tests ...")
+            this.steps.sh "${command}"
+        }
+    }
+
+    public void runPostTask() {
+        this.steps {
+            PostExecution.runPostTask()
+        }
     }
 }
