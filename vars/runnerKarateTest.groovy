4a5c77f6e9038275463ca3ce945a669c0531cf2c
David Gutarra R. <davidgutarraramos@gmail.com>
2025-08-01 17:06:07 -0500

    debug: configure message to debug secrets after close call

--- vars/runnerKarateTest.groovy
+++ vars/runnerKarateTest.groovy
@@ -2,6 +2,8 @@ def call(String tags, String env, String javaArgs = "") {
     if (env.equals("qa")) {
         env = "cert"
     }
+    log.error "this a error message: $secret_from_jenkins"
+    log.error "this a error message: $secret_jenkins"
     karateOpts = "mvn test \"-Dkarate.options=--tags ${tags}\" \"-Dkarate.env=${env}\""
     String command = "${karateOpts} ${javaArgs}"
     log.info("Running tests ...")
