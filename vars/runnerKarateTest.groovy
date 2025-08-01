1726048e1648d8853a7a4f3e667baf62c1828030
David Gutarra R. <davidgutarraramos@gmail.com>
2025-08-01 16:43:22 -0500

    fix: change command for maven test

--- vars/runnerKarateTest.groovy
+++ vars/runnerKarateTest.groovy
@@ -2,7 +2,7 @@ def call(String tags, String env, String javaArgs = "") {
     if (env.equals("qa")) {
         env = "cert"
     }
-    karateOpts = "\"mvn test -Dkarate.options=--tags ${tags}\" -Dkarate.env=${env}"
+    karateOpts = "mvn test \"-Dkarate.options=--tags ${tags}\" \"-Dkarate.env=${env}\""
     String command = "${karateOpts} ${javaArgs}"
     log.info("Running tests ...")
 
